a157f8316b00707aa54e02d5e93bca89daffa87d
Aldo Escobar <aldo@serimax.com.py>
2024-01-06 14:41:11 -0300

    [ADD] vehicule_fuels lines model

--- cost_fleet/models/__init__.py
+++ cost_fleet/models/__init__.py
@@ -4,3 +4,5 @@ from . import account_move_ext
 from . import product_product_ext
 from . import cost_fleet_vehicle_model_spare_cat
 from . import fleet_vehicle_model_ext
+from . import fleet_vehicle_ext
+from . import cost_fleet_vehicle_fuels
---
+++ cost_fleet/models/cost_fleet_vehicle_fuels.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+class CostFleetVehicleModelFuels(models.Model):
+   _name = 'cost.fleet.vehicle.model.fuels'
+   _description = 'Fuels enabled for model'
+   
+   vehicle_id = fields.Many2one(comodel_name='fleet.vehicle',string='Vehicle')
+   fuel_cat_id = fields.Many2one(
+        comodel_name='product.category',
+        string='Fuel Category',
+        domain="[('complete_name','like','Fuel /')]"
+   )
+   fuel_effic = fields.Float(string="Km/lt",required=True, default=10.0)
+
+
