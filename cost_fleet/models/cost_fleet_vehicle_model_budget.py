a1734da52a95a79d99231aec091d6a7c2ebce551
Aldo Escobar <aldo@serimax.com.py>
2024-01-08 07:38:22 -0300

    [ADD] lines TODO in budget

--- cost_fleet/models/cost_fleet_vehicle_model_budget.py
+++ cost_fleet/models/cost_fleet_vehicle_model_budget.py
@@ -5,7 +5,7 @@ class CostFleetModelBudget(models.Model):
    _name = 'cost.fleet.vehicle.model.budget'
    _description = 'Link the model Vehicle with consumables'
 
-   model_id = fields.Many2one('fleet.vehicle.model',string='Model', required=True)
+   model_id = fields.Many2one('fleet.vehicle.model',string='Model', required=True) #TODO Convertir a many2many
    name= fields.Char(string='Title')
    services_ids = fields.One2many(comodel_name='cost.fleet.vehicle.model.budget.services.line', inverse_name="budget_id", string="Services")
    consumable_cat_ids = fields.One2many(comodel_name='cost.fleet.vehicle.model.budget.consumables.line', inverse_name="budget_id",string="Consumables")
