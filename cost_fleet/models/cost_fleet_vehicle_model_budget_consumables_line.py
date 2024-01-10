437e828101bc4fd1ea6c6a8ad025fc7961c25a97
Aldo Escobar <aldo@serimax.com>
2024-01-10 19:38:44 -0300

    Agregue contrains unique y campos calculados de cost x km

--- cost_fleet/models/cost_fleet_vehicle_model_budget_consumables_line.py
+++ cost_fleet/models/cost_fleet_vehicle_model_budget_consumables_line.py
@@ -5,17 +5,25 @@ from odoo import models, fields
 class CostFleetVehicleModelBudgetConsumablesline(models.Model):
    _name = 'cost.fleet.vehicle.model.budget.consumables.line'
    _description = 'Line Links the model Budget with consumables'
+   _sql_constraints = [('spare_cat_uniq', 'unique(budget_id,spare_cat_id )', "Duplicate consumable or spare part line"), ]
 
    budget_id = fields.Many2one('cost.fleet.vehicle.model.budget',string='Budget', required=True, ondelete="cascade")
    spare_cat_id = fields.Many2one('cost.fleet.vehicle.model.spare.cat',string='Consumables', required=True,ondelete="restrict")
    qty = fields.Float(default=1.0,string='Quantity')   
    currency_id = fields.Many2one('res.currency',string='Currency', required=True)
-   value = fields.Monetary(string='Value', required=True)
+   value = fields.Monetary(string='Value', required=True,default=1.0)
    km_use = fields.Integer(string="life (km)", default=100)
+   cost_km = fields.Monetary(string='Cost/Km',compute='_compute_cost_by_km', stored=True)
    obs= fields.Text(string="Description")
+   
+   @api.depends('value','km_use')
+   def _compute_cost_by_km(self):
+      for line in self
+         if line.km_use = 0.0:
+            line.cost_km = 0.0
+         else:
+            line.cost_km = line.value / line.km_use
 
-   #TODO - crear campos compute del costo x km de la linea de consumible
-   #TODO Agregar constraint UNIQUE budget_id,spare_cat_id
 
 
 
--- cost_fleet/models/cost_fleet_vehicle_model_budget_services_line.py
+++ cost_fleet/models/cost_fleet_vehicle_model_budget_services_line.py
@@ -5,16 +5,23 @@ from odoo import models, fields
 class CostFleetVehicleModelBudgetServicesline(models.Model):
    _name = 'cost.fleet.vehicle.model.budget.services.line'
    _description = 'Line Links the model Budget with services'
+   _sql_constraints = [('service_uniq', 'unique(budget_id,service_type_id)', "Duplicate service line"), ]
 
    budget_id = fields.Many2one('cost.fleet.vehicle.model.budget',string='Budget', required=True,ondelete="cascade")
    service_type_id = fields.Many2one('fleet.service.type',string='Services', required=True,ondelete="restrict")
    currency_id = fields.Many2one('res.currency',string='Currency', required=True)
-   value = fields.Monetary(string='Value', required=True)
+   value = fields.Monetary(string='Value', required=True,default=1.0)
    km_use = fields.Integer(string="life (km)", default=100)
+   cost_km = fields.Monetary(string='Cost/Km',compute='_compute_cost_by_km', stored=True)
    obs = fields.Text(string="Description")
-
-    #TODO - crear campos compute del costo x km de la linea de servicio
-    #TODO Agregar constraint UNIQUE budget_id,service_type_id
+   
+   @api.depends('value','km_use')
+   def _compute_cost_by_km(self):
+      for line in self
+         if line.km_use = 0.0:
+            line.cost_km = 0.0
+         else:
+            line.cost_km = line.value / line.km_use
 
 
 
