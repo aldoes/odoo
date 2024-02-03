356c43f88298785ad5f150f38fbca5324f8361a7
Aldo Escobar <aldo@serimax.com.py>
2024-02-03 07:23:18 -0300

    [REF] abstract.purchase.fields, account.move,account.move.line : refactoring the computed fields and its methods

--- cost_fleet/models/__init__.py
+++ cost_fleet/models/__init__.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
+from . import account_move
+from . import account_move_line
+from . import product_category
+from . import account_fiscal_year
 from . import fleet_vehicle_model
 from . import cost_fleet_vehicle_values_line
 from . import fleet_vehicle
-from . import account_fiscal_year
 from . import abstract_purchase_fields
 from . import cost_fleet_vehicle_fuel
 from . import cost_fleet_vehicle_fuel_cats_line
@@ -11,8 +14,6 @@ from . import abstract_kmcost_fields
 from . import cost_fleet_vehicle_model_budget_services_line
 from . import cost_fleet_vehicle_model_budget_consumables_line
 from . import cost_fleet_vehicle_model_budget
-from . import product_category
 from . import cost_fleet_vehicle_model_spare
-from . import account_move
 
 
--- cost_fleet/models/abstract_purchase_fields.py
+++ cost_fleet/models/abstract_purchase_fields.py
@@ -6,32 +6,20 @@ class AbstractPurchaseFields(models.AbstractModel):
     _description = 'Abstract Model with last purchase data fields and its methods' 
     last_purch_cost = fields.Monetary(string='Last Cost',
                                       currency_field='last_purch_currency_id',
-                                      compute='_compute_last_cost', 
-                                      store=True)
-    last_purch_currency_id = fields.Many2one('res.currency',string='Last Purchase Currency')
-    last_purch_date = fields.Date(string="Date Last Purchase")
+                                      compute='_compute_last_adquire_cost')
+    last_purch_currency_id = fields.Many2one('res.currency',string='Last Purchase Currency', store=False)
+    last_purch_date = fields.Date(string="Date Last Purchase", store=False)
 
-    #TODO - mostrar el costo sin Impuesto
-    #TODO - Si no existe compra, Traer el costo Anotado en Seccion Supplierinfo sino cero
-
-    
-    def recalculate_last_cost(self):
-        self._compute_last_cost()
         
-        
-    def _compute_last_cost(self, date_limit= date.today()):
+    def _compute_last_adquire_cost(self, date_limit= date.today()):
         for record in self:
-            purchase = self.env['account.move'].get_last_purchase_line(record.product_id, date_limit)
-            if purchase:
-                record.last_purch_cost = purchase.price_unit
-                record.last_purch_currency_id = self.env.company.currency_id
-                record.last_purch_date = purchase.move_id.invoice_date
-            else:
-                record.last_purch_cost = 0.0
-                record.last_purch_currency_id = self.env.company.currency_id
-                record.last_purch_date = date_limit     
-    
-    
+            purchase = self.env['account.move'].get_last_purchased_line_to_date(record.product_id, date_limit)
+            record.last_purch_cost = purchase.price_unit 
+            record.last_purch_currency_id = purchase.currency_id if purchase.currency_id.id else self.env.company.currency_id
+            record.last_purch_date = purchase.move_id.invoice_date if purchase.move_id.invoice_date else date_limit
+        #TODO - mostrar el costo sin Impuesto
+        #TODO - Si no existe compra, Traer el costo Anotado en Seccion Supplierinfo sino cero
+
     def action_purchase_history(self):
         self.ensure_one()
         action = self.env["ir.actions.actions"]._for_xml_id("purchase.action_purchase_history")
