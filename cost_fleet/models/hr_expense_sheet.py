351181e7623658f7c4d78ff0ecfe5a3933d5630b
Aldo Escobar <aldo@serimax.com.py>
2024-02-11 10:18:20 -0300

    [ADD]hr.expense.sheet : Budgets import and currency

--- cost_fleet/models/hr_expense_sheet.py
+++ cost_fleet/models/hr_expense_sheet.py
@@ -4,8 +4,11 @@ from odoo import api, fields, models, _
 class HrExpenseSheet(models.Model):
     _inherit = "hr.expense.sheet"
 
-def get_last_expense_line_to_date(self, product, date_limit= date.today()):      
-            product.ensure_one()
-            '''Get last expense line of active and done expense before or on limit date or return empty record  '''
-            return self.env['hr.expense'].get_last_expense_line_to_date(product,date_limit)
+    budget_expen = fields.Monetary(string="Budget", currency_field="budg_currency_id")
+    budg_currency_id = fields.Many2one('res.currency',string='Currency', store=False)
+
+    def get_last_expense_line_to_date(self, product, date_limit= date.today()):      
+                product.ensure_one()
+                '''Get last expense line of active and done expense before or on limit date or return empty record  '''
+                return self.env['hr.expense'].get_last_expense_line_to_date(product,date_limit)
 
