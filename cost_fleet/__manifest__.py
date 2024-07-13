90a6b6d095178622241ce08863da97f410d384a0
Aldo Escobar <aldo@serimax.com.py>
2024-07-13 18:59:08 -0400

    Reinicio

--- cost_fleet/__manifest__.py
+++ cost_fleet/__manifest__.py
@@ -14,22 +14,23 @@ Module for Cost of Fleet
     'version': '17.0.0.3.0',
 
     # any module necessary for this one to work correctly
-    #'depends': ['base','purchase','fleet','hr'],.
-    'depends': ['base','purchase','fleet',"hr_expense",'account_fiscal_year'],
+    'depends': ['base','account_fiscal_year','purchase','fleet'],
+    #'depends': ['base','purchase','fleet',"hr_expense",'account_fiscal_year'],
+   
 
     # always loaded
     "data": [
-        "security/ir.model.access.csv",
+        # "security/ir.model.access.csv",
         "data/cost_fleet_vehicle_data.xml",
         "data/cost_fleet_fuel_data.xml",
-        "data/cost_fleet_spare_cat_data.xml",
-        "data/product.category.csv",
-        "views/account_fiscal_year_views.xml",
-        "views/product_category_views.xml",
-        "views/cost_fleet_vehicle_model_spare_view.xml",
-        "views/cost_fleet_vehicle_fuel_view.xml",
-        "views/fleet_views.xml",
-        "views/cost_fleet_vehicle_model_budget_view.xml",
+        # "data/cost_fleet_spare_cat_data.xml",
+        # "data/product.category.csv",
+        # "views/account_fiscal_year_views.xml",
+        # "views/product_category_views.xml",
+        # "views/cost_fleet_vehicle_model_spare_view.xml",
+        # "views/cost_fleet_vehicle_fuel_view.xml",
+        # "views/fleet_views.xml",
+        # "views/cost_fleet_vehicle_model_budget_view.xml",
         "views/cost_fleet_menu.xml",
     ],
     'license':"LGPL-3",
--- cost_fleet/models/__init__.py
+++ cost_fleet/models/__init__.py
@@ -1,24 +1,24 @@
 # -*- coding: utf-8 -*-
 
-from . import account_move
-from . import account_move_line
-from . import product_supplierinfo
-from . import product_category
+# from . import account_move
+# from . import account_move_line
+# from . import product_supplierinfo
+# from . import product_category
 from . import account_fiscal_year
-from . import hr_expense
-from . import hr_expense_sheet
-#from . import product_template in dev
+# from . import hr_expense
+# from . import hr_expense_sheet
+# #from . import product_template in dev
 from . import fleet_vehicle_model
 from . import cost_fleet_vehicle_values_line
 from . import fleet_vehicle
 from . import abstract_purchase_fields
 from . import cost_fleet_vehicle_fuel
 from . import cost_fleet_vehicle_fuel_cats_line
-from . import abstract_kmcost_fields
-from . import cost_fleet_vehicle_model_budget_services_line
-from . import cost_fleet_vehicle_model_budget_consumables_line
-from . import cost_fleet_vehicle_model_budget
-from . import cost_fleet_vehicle_model_spare
+# from . import abstract_kmcost_fields
+# from . import cost_fleet_vehicle_model_budget_services_line
+# from . import cost_fleet_vehicle_model_budget_consumables_line
+# from . import cost_fleet_vehicle_model_budget
+# from . import cost_fleet_vehicle_model_spare
 
 
 
