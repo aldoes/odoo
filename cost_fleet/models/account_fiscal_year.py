a12dd60ecf0fa53209c3209f7d4835d1ce3745c8
Aldo Escobar <aldo@serimax.com.py>
2024-07-24 20:16:35 -0400

    [ADD] year int field to fiscal_year model

--- cost_fleet/models/account_fiscal_year.py
+++ cost_fleet/models/account_fiscal_year.py
@@ -4,19 +4,30 @@
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 from odoo.osv import expression
+from datetime import date
 
 
 class AccountFiscalYear(models.Model):
     _inherit= "account.fiscal.year"
     _description = "Fiscal Year"
+    _order = "year desc"
 
+    # def _get_year_range(self,year_ini=date.today().year-10,year_qty=25):
+    #     year_list=[]
+    #     for i in range(year_qty):
+    #         year_list.append((year_ini+i,str(year_ini+i)))  
+    #     return year_list 
+    
     name = fields.Char(
         required =True,
         translate = True
     )
+
+    year = fields.Integer(string="year", default=date.today().year, required=True)
+
     is_open = fields.Boolean(
         default = True,
         required = True,
         string = "Is open?"
     )
-    
+
