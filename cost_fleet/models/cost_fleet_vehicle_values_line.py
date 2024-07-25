3ce79ce88f14094b884ea7669e8f16b8590621cc
Aldo Escobar <aldo@serimax.com.py>
2024-07-24 21:24:57 -0400

    [Add] get_list_value_lines_to_year function to cost.fleet.vehicle.values.line

--- cost_fleet/models/cost_fleet_vehicle_values_line.py
+++ cost_fleet/models/cost_fleet_vehicle_values_line.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields
+from datetime import date
 
 class CostFleetVehicleValuesline(models.Model):
    _name = 'cost.fleet.vehicle.values.line'
@@ -12,7 +13,30 @@ class CostFleetVehicleValuesline(models.Model):
    currency_id = fields.Many2one('res.currency',string='Currency', required=True)
    value = fields.Monetary(string='Value', required=True)
    km_use = fields.Integer(string="life (km)", default=100000)
-   
+
+   def get_last_value_to_year(self, vehicles, year=date.today().year):
+      # for vehicle in vehicles:
+      #       last_value_line = list_values_lines = self.get_list_value_lines_to_year(vehicle,year)
+      # # vehicle.ensure_one()
+      # 
+      # if len(last_value_line)==0:
+        
+       
+      # else:
+      #    if len(last_value_line) > 1:
+      #       last_value_line = list_values_lines.sorted(key=lambda r: r.fiscal_year_id, reverse=True)[0]
+
+      # return last_value_line 
+
+      pass
+      
+
+   def get_list_value_lines_to_year(self, vehicle, year=date.today().year):
+        vehicle.ensure_one()
+        domain = [('vehicle_id', '=', vehicle.id), 
+                ('fiscal_year_id.year', '<=', year)] 
+        return self.search(domain)
+
    #TODO Agregar constraint para bloquear cambio o eliminacion de registro por periodo contable abierto
 
 
