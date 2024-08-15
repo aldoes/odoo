1c40c8637895aa657c31381e069c143a619e38b8
Aldo Escobar <aldo@serimax.com.py>
2024-08-15 09:13:49 -0400

    [IMP] abstract.purchase.fields: _compute_last_adquire_cost improved and calculate cost in company.currency_id

--- cost_fleet/models/abstract_purchase_fields.py
+++ cost_fleet/models/abstract_purchase_fields.py
@@ -3,26 +3,29 @@ from odoo import api, fields, models, _
 
 class AbstractPurchaseFields(models.AbstractModel):
     _name ='abstract.purchase.fields'   
-    _description = 'Abstract Model with last purchase data fields and its methods' 
-    
+    _description = 'Abstract Model with last purchase data fields and its methods to models derivated from product.product model' 
+    #all cost must by in company.currency_id
     last_purch_cost = fields.Monetary(string='Last Cost',
                                       currency_field='last_purch_currency_id',
                                       compute='_compute_last_adquire_cost')
     last_purch_currency_id = fields.Many2one('res.currency',string='Last Purchase Currency', store=False)
     last_purch_date = fields.Date(string="Date Last Purchase", store=False)
-
         
     def _compute_last_adquire_cost(self, date_limit= date.today()):
-        for record in self:
-            purchaseLine = self.env['account.move'].get_last_purchased_line_to_date(record.product_id, date_limit)
-            supplierCost = self.env["product.supplierinfo"].get_highest_cost_supplierinfo_line(record.product_id)
-
-            record.last_purch_cost = (purchaseLine.price_unit if purchaseLine.move_id.invoice_date else (supplierCost.price if supplierCost.currency_id.id else 0.0))
-            record.last_purch_currency_id = (purchaseLine.currency_id if purchaseLine.currency_id.id else (supplierCost.currency_id if supplierCost.currency_id.id else self.env.company.currency_id))
-            record.last_purch_date = (purchaseLine.move_id.invoice_date if purchaseLine.move_id.invoice_date else date_limit)
+        # if not exists purchase record then get the Supplierinfo cost,if not, then is Zero
+        for record in self:        
+            record.last_purch_currency_id = self.env.company.currency_id 
+            record.last_purch_date = date_limit 
+            if (purchaseLine := self.env['account.move'].get_last_purchased_line_to_date(record.product_id, date_limit)):
+                record.last_purch_cost = purchaseLine.currency_id._convert(purchaseLine.price_unit,record.last_purch_currency_id)     
+                record.last_purch_date = purchaseLine.move_id.invoice_date 
+            elif (supplierCost := self.env["product.supplierinfo"].get_highest_cost_supplierinfo_line(record.product_id)):
+                record.last_purch_cost = supplierCost.currency_id._convert(supplierCost.price,record.last_purch_currency_id)                 
+            else:
+                record.last_purch_cost = 0.0
 
             #TODO - mostrar el costo sin Impuesto
-            #TODO - Si no existe compra, Traer el costo Anotado en Seccion Supplierinfo sino cero
+            
 
     def action_purchase_history(self):
         self.ensure_one()
