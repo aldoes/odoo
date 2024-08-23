3e1b8b35d1ea14bcea89299b43fd7c67f47e39a5
Aldo Escobar <aldo@serimax.com.py>
2024-08-22 20:14:12 -0400

    [IMP]cost.fleet.vehicle.model.spare : add get_spare_for_model function

--- cost_fleet/models/cost_fleet_vehicle_model_spare.py
+++ cost_fleet/models/cost_fleet_vehicle_model_spare.py
@@ -15,18 +15,18 @@ class CostFleetVehicleModelSpare(models.Model):
        auto_join=True, index=True, ondelete="cascade", required=True)
    km_use = fields.Integer(string="Estimate Use (km)", default=5000)
 
-#    def get_spare_by_cat_for_model(self, model, sortBy_highest_cost=False):
-#         model.ensure_one()
-        
-#         domain=[('model_ids','=',model.id),
+   def get_spare_for_model(self, model, sortBy_highest_cost=False):
+   #TODO - Implementar llamadas por category
+   # , category=None):
+        model.ensure_one()        
+        domain=[('model_ids','in', model.id),]
 #                ('categ_id.complete_name','like','Vehicular / '+ self.env.ref("cat_veh").name)]
-        
-#         list_spare=self.search(domain)
-#         if (sortBy_highest_cost and len(list_spare)>1):
-#             list_spare = list_spare.sorted(key=lambda r: r.last_cost, reverse=True)
-#         return list_spare
+        list_spare=self.search(domain)
+        if (sortBy_highest_cost and len(list_spare)>1):
+            list_spare = list_spare.sorted(key=lambda r: r.last_cost, reverse=True)
+        return list_spare
 
 
-   #TODO - tipo de producto repuesto, product category = spare
+   
 
 
