68d45627b5d8fe6138f44051404390c3ceecb880
Aldo Escobar <aldo@serimax.com.py>
2024-08-26 19:38:26 -0400

    [IMP]cost.fleet.vehicle.model.budget : add standard_price

--- cost_fleet/models/__init__.py
+++ cost_fleet/models/__init__.py
@@ -5,9 +5,6 @@ from . import account_move_line
 from . import product_supplierinfo
 from . import product_category
 from . import account_fiscal_year
-# from . import hr_expense
-# from . import hr_expense_sheet
-# #from . import product_template in dev
 from . import fleet_vehicle_model
 from . import cost_fleet_vehicle_values_line
 from . import fleet_vehicle
@@ -16,7 +13,6 @@ from . import cost_fleet_vehicle_fuel
 from . import cost_fleet_vehicle_fuel_cats_line
 from . import abstract_kmcost_fields
 from . import cost_fleet_vehicle_model_spare
-# from . import cost_fleet_vehicle_model_budget_services_line
 from . import cost_fleet_vehicle_model_budget_consumables_line
 from . import cost_fleet_vehicle_model_budget
 
--- cost_fleet/models/cost_fleet_vehicle_model_budget.py
+++ cost_fleet/models/cost_fleet_vehicle_model_budget.py
@@ -22,6 +22,17 @@ class CostFleetVehicleModelBudget(models.Model):
         store=True,
         ondelete='restrict',
     )
+  standard_price = fields.Monetary(string='Costo',
+  currency_field='currency_id'
+    # digits='Product Price', 
+    # groups="base.group_user"
+  )
+  currency_id = fields.Many2one(comodel_name='res.currency', default=lambda self: self.env.company.currency_id, string='Moneda')
+  supplier_taxes_id = fields.Many2many('account.tax',
+        string='Impuesto Proveedor', 
+        domain=[('type_tax_use', '=', 'purchase')],
+        default=lambda self: self.env.companies.account_purchase_tax_id or self.env.companies.root_id.sudo().account_purchase_tax_id,
+  )
   km_use = fields.Integer(string="km Estimados", default=5000)
   obs= fields.Text(string="Details")
   consum_cat_line_ids = fields.One2many(comodel_name='cost.fleet.vehicle.model.budget.consumables.line', 
