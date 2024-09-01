5b9726f94f6a8c5bcd3ea9808c1b9e5020a9c1bc
Aldo Escobar <aldo@serimax.com.py>
2024-09-01 17:26:30 -0400

    [FIX] cost.fleet.vehicle.values.line : error with empty result in get_list_value_lines_to_year

--- cost_fleet/models/cost_fleet_vehicle_values_line.py
+++ cost_fleet/models/cost_fleet_vehicle_values_line.py
@@ -18,23 +18,23 @@ class CostFleetVehicleValuesline(models.Model):
    def get_last_value_to_year(self, vehicle, year=date.today().year):    
       vehicle.ensure_one() 
       last_value_line = self.get_list_value_lines_to_year(vehicle,year)
-      if len(last_value_line)==0:
-            last_value_line=self.env['cost.fleet.vehicle.values.line'].new({
-               'vehicle_id': vehicle.id,
-               'value': vehicle.net_car_value or 1,
-               'currency_id': vehicle.currency_id or self.env.company.currency_id,
-               'km_use':  vehicle.km_use or 1
-            })
-      else:
-         if len(last_value_line)>1:
-            last_value_line = last_value_line.sorted(key=lambda r: r.fiscal_year_id.year, reverse=True)[0]
+      if (len(last_value_line)>1):
+         last_value_line = last_value_line.sorted(key=lambda r: r.fiscal_year_id.year, reverse=True)[0]
       return last_value_line 
 
    def get_list_value_lines_to_year(self, vehicle, year=date.today().year):
-        vehicle.ensure_one()
-        domain = [('vehicle_id', '=', vehicle.id), 
-                ('fiscal_year_id.year', '<=', year)] 
-        return self.search(domain)
+      vehicle.ensure_one()
+      domain = [('vehicle_id', '=', vehicle.id), 
+         ('fiscal_year_id.year', '<=', year)] 
+      value_list = self.env['cost.fleet.vehicle.values.line'].new({
+         'vehicle_id': vehicle.id,
+         'value': vehicle.net_car_value or 1,
+         'currency_id': vehicle.currency_id or self.env.company.currency_id,
+         'km_use':  vehicle.km_use or 1
+         })
+      if(result:=self.search(domain)):
+         value_list = result
+      return value_list
 
    #TODO Agregar constraint para bloquear cambio o eliminacion de registro por periodo contable abierto
 
