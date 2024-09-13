5f36e0c74aaf89a0aadd6c4ff6dda5b006c3997e
Aldo Escobar <aldo@serimax.com.py>
2024-09-12 20:50:08 -0400

    [IMP] abstract.purchase.fields : change algorithm to calculate actual cost without taxes

--- cost_fleet/models/abstract_purchase_fields.py
+++ cost_fleet/models/abstract_purchase_fields.py
@@ -12,25 +12,28 @@ class AbstractPurchaseFields(models.AbstractModel):
     last_info_date = fields.Date(string="Cost Date", store=False)
         
     def _compute_last_adquire_cost(self, date_limit= date.today()):
-        #TODO - calculate cost whitout taxes
-        # if not exists purchase record then get the Supplierinfo cost,if not, then is Zero
-        for record in self:        
-            record.info_currency_id = self.env.company.currency_id 
-            record.last_info_date = date_limit 
+        #calculate cost whitout taxes
+        for record in self:   
+            record.info_currency_id = info_currency_id = self.env.company.currency_id   
+            last_info_date = date_limit
+            last_cost = record.standard_price
+
             if (purchaseLine := self.env['account.move'].get_last_purchased_line_to_date(record.product_id, date_limit)):
-                record.last_cost = purchaseLine.currency_id._convert((purchaseLine.price_subtotal/purchaseLine.quantity),record.info_currency_id)     
-                record.last_info_date = purchaseLine.move_id.invoice_date 
-            elif (supplierCost := self.env["product.supplierinfo"].get_highest_cost_supplierinfo_line(record.product_id)):
-                taxes_res = record.supplier_taxes_id.compute_all(supplierCost.price,quantity=1.0,currency=supplierCost.currency_id,product=record.product_id)
-                price_withoutTax = taxes_res['total_excluded']
-                record.last_cost = supplierCost.currency_id._convert(price_withoutTax,record.info_currency_id)                 
-            else:
-                taxes_res = record.supplier_taxes_id.compute_all(record.standard_price,quantity=1.0,currency=record.currency_id,product=record.product_id)
-                record.last_cost = record.currency_id._convert(taxes_res['total_excluded'],record.info_currency_id)
-
+                last_cost = purchaseLine.price_unit
+                info_currency_id = purchaseLine.currency_id
+                last_info_date = purchaseLine.move_id.invoice_date
             
-            
+            if (supplierCost := self.env["product.supplierinfo"].get_highest_cost_supplierinfo_line(record.product_id)):  
+                if not(purchaseLine and (purchaseLine.move_id.invoice_date > supplierCost.write_date.date())):  
+                    last_cost = supplierCost.price
+                    info_currency_id = purchaseLine.currency_id
+                    last_info_date = supplierCost.write_date.date()
 
+            taxes_res = record.supplier_taxes_id.compute_all(last_cost,quantity=1.0,currency=info_currency_id,product=record.product_id)
+            record.last_cost = info_currency_id._convert(taxes_res['total_excluded'],record.info_currency_id)
+            record.last_info_date = last_info_date         
+          
+            
     def action_purchase_history(self):
         self.ensure_one()
         action = self.env["ir.actions.actions"]._for_xml_id("purchase.action_purchase_history")
